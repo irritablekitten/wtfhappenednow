0d0dc670123ff8dc395c5d3bb707af49a02db24b
Samuel Defazio <irritablekitten@gmail.com>
2018-02-09 18:40:31 -0800

    Fix errors, refactor

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
 const damerau = require('damerau-levenshtein');
 const isomorphicFetch = require('isomorphic-fetch');
-const express = require('express');
-const mongoose = require('mongoose');
-const keys = require('./keys/keys');
+const moment = require('moment');
 const schedule = require('node-schedule');
 const admin = require("firebase-admin");
 const serviceAccount = require("./keys/wtfhappenednow-server-firebase-adminsdk-q74se-ae52909ff9.json");
-const app = express();
+const keys = require('./keys/keys');
+const token = keys.key;
 const filters = ['to', 'for', 'the', 'in', 'a', 'and', 'to', 'of', 'but', 'from', 'at', 'when', ',', '', '|', 'is', 'are', 'an', 'will', 'be', '-', '\\', 'by', 'on', 'as'];
 let sourceArray = [];
 let titleArray = [];
 let compareArray = [];
-let sortWords = [];
 let countSources = [];
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://wtfhappenednow-server.firebaseio.com"
 });
-
 var db = admin.database();
 var ref = db.ref("/newsdata/1d3V3Yd3CRen8aqYTrXx/");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-});
 
 //gets all news sources by id
 let scrape = async () => {
@@ -36,35 +28,40 @@ let scrape = async () => {
 
 //obtains top articles per source id
 let addTitles = async (obj) => {
-    const token = keys.key;
-    let resTwo = await fetch(
+    let res = await fetch(
         `https://newsapi.org/v2/top-headlines?sources=${obj}&apiKey=${token}`)
-    return await resTwo.json(); 
+    return await res.json(); 
 }
 
-const TitleScraper = () => {
+const sourceMap = () => {
+    let newArray = [];
     scrape().then((res) =>  {
         if (res.sources !== undefined) {
             res.sources.map(function (obj) {             
-                sourceArray.push(obj);                 
+                newArray.push(obj);              
             });    
         }
-        
-        sourceArray.map(function (obj) {
-            addTitles(obj.id).then((resTwo) => {
-                if (resTwo.articles !== undefined) {
-                    resTwo.articles.map(function (objTwo) {
-                        let article = {
-                            id: obj.id,
-                            title: objTwo.title,
-                            url: objTwo.url
-                        };
-                        titleArray.push(article);
-                    }); 
-                } 
-            }).catch(err => console.error(err)) 
-        });
     }).catch(err => console.error(err))
+    return newArray;
+}
+
+const titleMap = (titles) => {
+    let newArray = [];
+    titles.map(function (obj) {
+        addTitles(obj.id).then((res) => {
+            if (res.articles !== undefined) {
+                res.articles.map(function (objTwo) {
+                    let article = {
+                        id: obj.id,
+                        title: objTwo.title,
+                        url: objTwo.url
+                    };
+                    newArray.push(article);
+                }); 
+            } 
+        }).catch(err => console.error(err)) 
+    });
+    return newArray;
 }
 
 //https://stackoverflow.com/questions/17313268/idiomatically-find-the-number-of-occurrences-a-given-value-has-in-an-array
@@ -82,11 +79,10 @@ class Counter extends Map {
     }
   }
 
-
 //finds matches between titles by comparing one title in titleArray from the compareFunction loop to every title within the same array, while logging high-scoring results and ignoring exact matches
 const levenshteinFunction = (i) => {
-    let titleArrayCopy = titleArray;
-    titleArrayCopy.map(function (obj) {
+    let copy = titleArray;
+    copy.map(function (obj) {
         let test = damerau(obj.title, i.title);
 
         let comparison = {
@@ -104,8 +100,9 @@ const levenshteinFunction = (i) => {
 
 //provides the top level loop for finding matches in titleArray with the damerau-levenshtein algorith
 const compareFunction = () => {
-    let titleArrayCopy = titleArray;
-   titleArrayCopy.map(function (i) {
+    compareArray = [];
+    let copy = titleArray;
+    copy.map(function (i) {
         levenshteinFunction(i);
     });
 }
@@ -113,12 +110,14 @@ const compareFunction = () => {
 //takes results pushed to compareArray and splits each title into individual words to find commonality
 const splitFunction = (splitThis) => {
     let copy = splitThis;
-    let newArray = []
+    let newArray = [];
+    let countArray = [];
     copy.map(function (obj) {
         let post = obj.first.split(' ');
         newArray.push(post);
-        countSources.push(obj.source);
+        countArray.push(obj.source);
      });
+     countSources = countArray;
      return newArray;
 }
 
@@ -142,12 +141,15 @@ const arrayFilter = (results) => {
 }
 
 const sendToDB = (wordscounted, sourcescounted) => {
+    let dateTime = moment().format('LLL');
     let postsRef = ref.child("results");
     postsRef.push({
         wordcount: wordscounted,
         sourcecount: sourcescounted,
-        date: admin.database.ServerValue.TIMESTAMP
+        date: admin.database.ServerValue.TIMESTAMP,
+        fulldate: dateTime
       });
+    console.log("Pushed to DB. ", dateTime);
 }
 
 const getFromDB = () => {
@@ -158,39 +160,37 @@ const getFromDB = () => {
 
 const main = () => {
     //scheduled to pull sources and top articles from each source at the 58 minute mark every hour or 53 for pi3
-    let runScrape = schedule.scheduleJob('53 * * * *', function(){
-        TitleScraper();
-      });
+    let runScrape = schedule.scheduleJob('33 * * * *', function(){
+        sourceArray = sourceMap();
+    });
+
+    let getArticles = schedule.scheduleJob('34 * * * *', function(){
+        titleArray = titleMap(sourceArray);
+    });
+
     //scheduled to process data at 59 minutes of every hour or 54 for pi3
-      let everythingElse = schedule.scheduleJob('54 * * * *', function(){
+    let everythingElse = schedule.scheduleJob('35 * * * *', function(){
         compareFunction();
-            let splitArray = splitFunction(compareArray);
-            let results = arrayFixer(splitArray);
-            let countArray = arrayFilter(results);     
-            let counter = new Counter(countArray);
-            delete counter.key;
-            sortWords = Array.from(counter);
-            sortWords.sort(function(a, b) {
-                return b[1] - a[1];
-            });
-            console.log(sortWords);
-            
-            let printSources = new Counter(countSources);
-            delete printSources.key;
-            let sortSources = Array.from(printSources);
-            sortSources.sort(function(a, b) {
-                return b[1] - a[1];
-            });
-            console.log(sortSources);
-            sendToDB(sortWords, sortSources);
-      });
+        let splitArray = splitFunction(compareArray);
+        let results = arrayFixer(splitArray);
+        let countArray = arrayFilter(results);     
+        let counter = new Counter(countArray);
+        delete counter.key;
+        let sortWords = Array.from(counter);
+        sortWords.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        let printSources = new Counter(countSources);
+        delete printSources.key;
+        let sortSources = Array.from(printSources);
+        sortSources.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        sendToDB(sortWords, sortSources);
+    });
 }
 
-app.get('/wordcount', function (req, res) {
-    res.send(JSON.stringify(sortWords));
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
 
 main();
